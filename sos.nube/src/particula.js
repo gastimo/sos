bc0abb28a6dd12e47398ab907e6b8216f68f2077
Gaston <gaston.martino@gmail.com>
2025-08-01 10:51:29 -0300

    Cambios menores en el código de la "Partícula"

--- sos.nube/src/particula.js
+++ sos.nube/src/particula.js
@@ -18,15 +18,20 @@ const ACELERACION_MAX = 0.000005;
 
 /**
  * Particula
- * Objeto que representa a una particula simple que se desplaza en el plano.
+ * Objeto que representa a una particula simple 
+ * que se desplaza en el plano 2D.
  */
 function Particula(S) {
-    let _posicion    = {x: S.O.S.aleatorio(POSICION_MIN,   POSICION_MAX), y: S.O.S.aleatorio(POSICION_MIN,   POSICION_MAX)};
-    let _velocidad   = {x: aleatorio(VELOCIDAD_MIN,   VELOCIDAD_MAX),     y: aleatorio(VELOCIDAD_MIN,   VELOCIDAD_MAX)};
-    let _aceleracion = {x: aleatorio(ACELERACION_MIN, ACELERACION_MAX),   y: aleatorio(ACELERACION_MIN, ACELERACION_MAX)};
+    let _posicion    = {x: S.O.S.aleatorio(POSICION_MIN,    POSICION_MAX), 
+                       y: S.O.S.aleatorio(POSICION_MIN,    POSICION_MAX)};
+    let _velocidad   = {x: S.O.S.aleatorio(VELOCIDAD_MIN,   VELOCIDAD_MAX,   true),     
+                       y: S.O.S.aleatorio(VELOCIDAD_MIN,   VELOCIDAD_MAX,   true)};
+    let _aceleracion = {x: S.O.S.aleatorio(ACELERACION_MIN, ACELERACION_MAX, true),   
+                       y: S.O.S.aleatorio(ACELERACION_MIN, ACELERACION_MAX, true)};
     let _origen      = _copiar(_posicion);
     let _rebotar     = false;
     let _maxVelocidad;
+    let _maxAceleracion;
     let _duracion;
     
     function origen() {
@@ -62,7 +67,17 @@ function Particula(S) {
     }
     
     function velocidadMaxima(limite) {
-        _maxVelocidad = limite;
+        if (limite !== undefined) {
+            _maxVelocidad = Math.abs(limite);
+        }
+        return _maxVelocidad;
+    }
+    
+    function aceleracionMaxima(limite) {
+        if (limite !== undefined) {
+            _maxAceleracion = Math.abs(limite);
+        }
+        return _maxAceleracion;
     }
     
     function distancia(particula) {
@@ -88,22 +103,38 @@ function Particula(S) {
         }
         return _rebotar;
     }
-    
-    function aleatorio(min, max) {
-        return S.O.S.aleatorio(min, max) * Math.sign(S.O.S.aleatorio(-1, 1)) ?? 1;
-    }
-    
+
     function actualizar() {
-        // Actualizar velocidad y posición
+        // 1. ACELERACIÓN
+        // Primero, se controla que la aceleración no supere el máximo
+        // permitido. Luego se suma la aceleración a la velocidad.
+        // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        if (_maxAceleracion !== undefined) {
+            _ajustarMagnitud(_aceleracion, _maxAceleracion);
+        }        
         _adicionar(_velocidad, _aceleracion);
-        _velocidad.x = Math.min(_velocidad.x, _maxVelocidad ?? _velocidad.x);
-        _velocidad.y = Math.min(_velocidad.y, _maxVelocidad ?? _velocidad.y);
+        
+        // 2. VELOCIDAD
+        // Se controla que la velocidad actualizada no supere el máximo
+        // permitido. Luego se le suma la velocidad a la posición.
+        // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        if (_maxVelocidad !== undefined) {
+            _ajustarMagnitud(_velocidad, _maxVelocidad);
+        }
         _adicionar(_posicion, _velocidad);
+
+        // 3. DURACIÓN
+        // Se decrementa el contador de veces que la partícula ha sido
+        // actualizada para llevar la cuenta del tiempo transcurrido.
+        // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         if (_duracion) {
             _duracion--;
         }
         
-        // Evitar que se salga de coordenadas mediante el rebote
+        // 4. REBOTE
+        // Si se debe controlar el rebote, se verifica que las coordenadas
+        // <x, y> no traspasen los bordes normalizados del plano <0, 1>.
+        // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         if (_rebotar) {
             if (_posicion.x <= 0 || _posicion.x >= 1) {
                 _posicion.x = _posicion.x <= 0 ? 0 : 1;
@@ -138,16 +169,28 @@ function Particula(S) {
         return a;
     }
     
+    function _magnitud(a, b) {
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+    
+    function _ajustarMagnitud(v, m) {
+        let _mag = _magnitud(v.x, v.y);
+        if (_mag > m) {
+            v.x = v.x * m / _mag;
+            v.y = v.y * m / _mag;
+        }
+    }
+    
     return {origen,
            posicion,
            velocidad,
            aceleracion,
            velocidadMaxima,
+           aceleracionMaxima,
            distancia,
            direccion,
            duracion,
            rebotar,
-           aleatorio,
            actualizar,
            caducada};
 }
